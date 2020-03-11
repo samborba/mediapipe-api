61f7e5dce539c23d7509578103fcd420dd2575bb
samborba <bf_samuel@hotmail.com>
2020-03-11 14:30:59 -0300

    Requirements

--- src/app.py
+++ src/app.py
@@ -5,8 +5,5 @@ from api import create_app
 
 logging.basicConfig(format='%(asctime)s - %(message)s', level=logging.INFO)
 
-if not os.path.isdir(os.path.abspath("/resources")):
-    os.mkdir("/resources")
-
 app = create_app("development")
 app.run()
